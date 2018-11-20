5cc13a4e4af57735aeb2bbebc7b3c982cfcce855
Nicky Chen <nicky.chen0@gmail.com>
2018-11-19 21:15:22 -0800

    endpoints

--- friends/friendRoute.js
+++ friends/friendRoute.js
@@ -15,10 +15,46 @@ router.post("/", async (req, res) => {
   const newFriend = new Friend(req.body);
   try {
     const savedFriend = await newFriend.save();
+    if (!savedFriend) {
+      res
+        .status(500)
+        .json({ errorMessage: "There was an error while saving the friend to the database." });
+    }
     res.status(201).json(savedFriend);
   } catch (err) {
-    res.status(400).json({ error: err.message });
+    res.status(404).json({ err: err.message });
   }
 });
+
+router.get("/:id", async (req, res) => {
+  try {
+    const singleFriend = await Friend.findById(req.params.id);
+    if (singleFriend) {
+      res.status(200).json(singleFriend);
+    } else {
+      res.status(404).json("That id could not be found.");
+    }
+  } catch (err) {
+    res.status(400).json({ err: err.message });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  if (!req.body.singleName && !req.body.lastName && !req.body.age) {
+    res.status(400).json("Need something to edit.");
+  }
+  try {
+    const updatedFriend = await Friend.findByIdAndUpdate(req.params.id, req.body);
+    if (updatedFriend) {
+      res.status(201).json(updatedFriend);
+    } else {
+      res.status(404).json("That id could not be found");
+    }
+  } catch (err) {
+    res.status(400).json({ err: err.message });
+  }
+});
+
+router.delete("/:id", (req, res) => {});
 
 module.exports = router;
